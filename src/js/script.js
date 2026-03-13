a5c64188d094986f32a86a7ad9c1607a3618edc3
mepeha <151367905+mepeha@users.noreply.github.com>
2026-03-13 15:00:43 +0300

    main-page v2

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,46 @@
 $(function () {
+    const $body = $('body');
+    const $burger = $('.header__burger');
+    const $mobileMenu = $('.mobile-menu');
+    const $mobileOverlay = $('.mobile-menu__overlay');
     const $endHomeSlider = $('.end-home__area');
+
+    if ($burger.length && $mobileMenu.length) {
+        const openMenu = function () {
+            $body.addClass('menu-open');
+            $burger.attr('aria-expanded', 'true');
+            $mobileMenu.attr('aria-hidden', 'false');
+        };
+
+        const closeMenu = function () {
+            $body.removeClass('menu-open');
+            $burger.attr('aria-expanded', 'false');
+            $mobileMenu.attr('aria-hidden', 'true');
+        };
+
+        $burger.on('click', function () {
+            if ($body.hasClass('menu-open')) {
+                closeMenu();
+                return;
+            }
+            openMenu();
+        });
+
+        $mobileOverlay.on('click', closeMenu);
+        $mobileMenu.find('a').on('click', closeMenu);
+
+        $(document).on('keydown', function (e) {
+            if (e.key === 'Escape') {
+                closeMenu();
+            }
+        });
+
+        $(window).on('resize', function () {
+            if (window.innerWidth > 1400) {
+                closeMenu();
+            }
+        });
+    }
 
     if ($endHomeSlider.length && !$endHomeSlider.hasClass('slick-initialized')) {
         $endHomeSlider.slick({
@@ -24,7 +65,7 @@ $(function () {
                     breakpoint: 768,
                     settings: {
                         slidesToShow: 1,
-                        arrows: false,
+                        arrows: false
                     }
                 }
             ]
